ffa5b5a611d95ee0aa0a4c9cf304643efc9a2032
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-28 15:33:04 +0600

    Display object from DataBase to HTML

--- personalPortfolio/urls.py
+++ personalPortfolio/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-
+import portfolio.views as portfolio
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', portfolio.home, name='home'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
