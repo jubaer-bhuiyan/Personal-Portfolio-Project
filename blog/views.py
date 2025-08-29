f61a11173e516d6a733b954be81b7b8b502d88f5
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-08-29 10:59:07 +0600

    BLog Details

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def allBlogs(request):
     Blogs = Blog.objects.order_by('-published_date')[:5]
     return render(request, 'blog/allBlogs.html', {'blogs': Blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
